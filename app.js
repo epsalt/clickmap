26ff74caf2209ffc83f325b82f525e388f9a22f2
epsalt <evan.saltman@gmail.com>
2020-07-08 19:14:34 -0600

    moved express app code to one file

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 const express = require("express");
 const geojson = require("geojson");
+const sqlite3 = require("sqlite3").verbose();
 const hri = require("human-readable-ids").hri;
-const db = require("./database.js");
+const path = require("path");
 
 const app = express();
+const dbname = "db.sqlite";
 
 app.set("port", process.env.PORT || 3000);
-app.use(express.static("dist"));
+app.use("/", express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 app.get("/api/", (req, res) => res.json({ message: "Ok" }));
 
+let db = new sqlite3.Database(dbname, (err) => {
+  if (err) {
+    console.error(err.message);
+    throw err;
+  } else {
+    db.run(
+      `CREATE TABLE map (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            session text,
+            timestamp integer,
+            lat real,
+            lng real
+            )`,
+      (err) => {
+        if (err) {
+          console.log(err.message);
+        }
+      }
+    );
+  }
+});
+
 app.get("/api/coords/", (req, res) => {
-  var sql = "SELECT * from map";
-  var params = [];
+  let sql = "SELECT * from map";
+  let params = [];
 
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -31,14 +55,14 @@ app.get("/api/id", (req, res) =>
 );
 
 app.post("/api/coords/", (req, res) => {
-  var sql = "INSERT INTO map (session, timestamp, lat, lng) VALUES (?,?,?,?)";
-  var data = {
+  let sql = "INSERT INTO map (session, timestamp, lat, lng) VALUES (?,?,?,?)";
+  let data = {
     session: req.body.session,
     timestamp: req.body.timestamp,
     lat: req.body.coords[1],
     lng: req.body.coords[0],
   };
-  var params = [data.session, data.timestamp, data.lat, data.lng];
+  let params = [data.session, data.timestamp, data.lat, data.lng];
 
   db.run(sql, params, (err, result) => {
     if (err) {
